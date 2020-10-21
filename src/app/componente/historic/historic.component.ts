188d310f55126e748fe550d71706556bdb0f4d32
Landson <randelsilva1990@gmail.com>
2020-10-21 01:03:10 -0300

    criando retorno para paginacao do listar historico no service.

--- src/app/componente/historic/historic.component.ts
+++ src/app/componente/historic/historic.component.ts
@@ -1,6 +1,7 @@
 import { HistoricService } from './../../service/historic/historic.service';
 import { Component, OnInit } from '@angular/core';
 import { Historic } from 'src/app/models/historic';
+import { UserService } from 'src/app/service/user/user.service';
 
 @Component({
   selector: 'app-historic',
@@ -13,24 +14,14 @@ export class HistoricComponent implements OnInit {
   rows: any[] = [];
 
   constructor(
-    private historicService: HistoricService
+    private historicService: HistoricService,
+    private userService: UserService
   ) { }
 
   ngOnInit(): void {
     this.historicService.listHistoricClient(1).subscribe((resp) => this.historicList = resp)
   }
 
-   /** Gets the total cost of all transactions. */
-   getTotalCost() {
-    //return this.transactions.map(t => t.cost).reduce((acc, value) => acc + value, 0);
-  }
 
-  groupColumns(photos: Historic[]) {
-    const newRows = [];
 
-    for(let index = 0; index < photos.length; index+=3) {
-      newRows.push(photos.slice(index, index + 3));
-    }
-    return newRows;
-  }
 }
