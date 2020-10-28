2d5b5f57a572c97af2e37142eceb65866dde5b61
Landson <randelsilva1990@gmail.com>
2020-10-28 14:21:33 -0300

    fazendo deploy da aplicacao

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+
+const app = express();
+
+const PORT = process.env.PORT || 8080;
+
+app.use(express.static(__dirname + 'dist/MultBank-spa'));
+
+app.get('/*', (req, res) => {
+  res.sendFile(__dirname + 'dist/MultBank-spa/index.html')
+})
+
+app.listen(PORT, () => {
+  console.log('Servidor ligado na porta ' + PORT)
+})
